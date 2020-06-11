c1ff221447b3d93903e09654479433acdcb0f0dd
Greyson Storm <greysonstorm@gmail.com>
2020-06-11 19:38:17 +0100

    .

--- newlib/libc/include/machine/time.h
+++ newlib/libc/include/machine/time.h
@@ -1,10 +1,14 @@
 #ifndef	_MACHTIME_H_
 #define	_MACHTIME_H_
 
-#if defined(__rtems__) || defined(__VISIUM__) || defined(__riscv) || (__psp__)
+#if defined(__rtems__) || defined(__VISIUM__) || defined(__riscv)
 #define _CLOCKS_PER_SEC_ 1000000
 #elif defined(__aarch64__) || defined(__arm__) || defined(__thumb__)
 #define _CLOCKS_PER_SEC_ 100
+#endif
+
+#if defined(__psp__)
+#define _CLOCKS_PER_SEC_  1000000
 #endif
 
 #ifdef __SPU__
